6b7bf8cbc1ee12ddaab3e37e1ffd64b8a62ec2d0
Onur <onurbelek@outlook.com>
2022-05-29 13:19:01 +0100

    created the nav-bar

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ CORS(app)
 
 @app.route('/')
 def landing_page():
-    return render_template("landing_page.html")
+    return render_template("base.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
