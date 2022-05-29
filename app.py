61b6a829d3c94160413b488199e498609ae34809
Onur <onurbelek@outlook.com>
2022-05-29 11:32:12 +0100

    adding the first route with a static template!

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, jsonify, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_cors import CORS
 from controllers import cats
 from werkzeug import exceptions
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route('/')
+def landing_page():
+    return render_template("landing_page.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
