3d65ef2b59a2891b6402ca51745f3a1574a34721
“iAmash412” <aafthab.ashraff@gmail.com>
2022-05-29 16:49:20 +0100

    more routes

--- app.py
+++ app.py
@@ -6,13 +6,19 @@ from werkzeug import exceptions
 app = Flask(__name__)
 CORS(app)
 
+
+
 @app.route('/')
 def landing_page():
     return render_template("base.html")
 
 @app.route('/user/<name>')
 def user(name):
-    return render_template("user.html", name=name)
+    return render_template("user.html", user_name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
 
 
 if __name__ == "__main__":
